3c2b8459643568da871e304e741c374d434281dc
Ginso <ginso89@gmail.com>
2026-03-24 14:27:34 +0100

    up

--- app/src/main/java/com/example/danceplayer/MainActivity.kt
+++ app/src/main/java/com/example/danceplayer/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
